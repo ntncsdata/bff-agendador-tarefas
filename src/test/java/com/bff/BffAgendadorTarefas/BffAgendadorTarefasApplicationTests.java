4c589e0f42daed824556dd52e8710213982354a3
Natan <natancintra28@gmail.com>
2026-07-06 12:03:34 -0300

    Adicionado esqueleto do projeto

---
+++ src/test/java/com/bff/BffAgendadorTarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bff.BffAgendadorTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
