b5c56d4dd35eea7ebd7533f377c481c90238cf30
Natan <natancintra28@gmail.com>
2026-07-07 14:36:34 -0300

    Adicionado client de email

---
+++ src/main/java/com/bff/BffAgendadorTarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,11 @@
+package com.bff.BffAgendadorTarefas.infrastructure.client;
+
+import com.bff.BffAgendadorTarefas.business.dto.out.TarefasDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+}
